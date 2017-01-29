afc486173a733620ab936732468d2c9d0dad6ecd
ali <a.fareghbal@hamitrip.com>
2017-01-29 17:40:23 +0330

    init

---
+++ image/rootfs/usr/local/bin/start.sh
@@ -0,0 +1,5 @@
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables --table nat --append POSTROUTING --out-interface eth0 --jump MASQUERADE
+iptables -A INPUT -udp -m udp --dport 1194 -j ACCEPT
+iptables-save > /etc/iptables.conf
+supervisord
